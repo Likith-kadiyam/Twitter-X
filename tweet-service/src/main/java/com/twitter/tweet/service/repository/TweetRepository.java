ef3c9f466a23e060460f22e1f4c677b8c7776aab
Md Quayem Ashraf <quayemanas@gmail.com>
2026-07-02 16:16:31 +0530

    abc

--- tweet-service/src/main/java/com/twitter/tweet/service/repository/TweetRepository.java
+++ tweet-service/src/main/java/com/twitter/tweet/service/repository/TweetRepository.java
@@ -17,10 +17,7 @@ public interface TweetRepository extends JpaRepository<Tweet,Long> {
             SELECT t
                FROM Tweet t
                WHERE t.createdAt >= :fromTime
-<<<<<<< HEAD
                AND (  t.likeCount > 0 OR t.replyCount > 0 OR t.retweetCount > 0)
-=======
->>>>>>> main
          """)
     List<Tweet> findByCreatedAtAfter(LocalDateTime fromTime);
 }
